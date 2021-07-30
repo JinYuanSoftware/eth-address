63c64c8a6c09f83253d00bee648b8bebef601af6
weizheng <jsonwei@163.com>
2021-07-31 04:17:43 +0800

    http

--- run.php
+++ run.php
@@ -2,5 +2,9 @@
 require 'vendor/autoload.php';
 use src\geth\newAccount;
 
-$obj = new newAccount();
-$obj->newAddress();
+
+$nodeUrl = "http://103.39.233.125:8643";
+$keyPath = "/data/geth/keystore";
+$num = 1000;
+$obj = new newAccount($nodeUrl);
+$obj->newAddress($keyPath,$num);
--- src/geth/newAccount.php
+++ src/geth/newAccount.php
@@ -4,19 +4,22 @@ namespace src\geth;
 
 class newAccount
 {
-    public function newAddress()
+    public $nodeUrl;
+    public function __construct($nodeUrl)
     {
-        $n = 1000;
-        while($n > 0){
-            $n -= 1;
+        $this->nodeUrl = $nodeUrl;
+    }
+    public function newAddress($keyPath,$num)
+    {
+        while($num > 0){
+            $num -= 1;
             $password = $this->GetRandStr(8);
-            exec("chmod +x ./src/sh/newAccount.sh",$rep);
-            exec("sh ./src/sh/newAccount.sh $password",$rep);
+            $rep = $this->post($this->nodeUrl,$this->createDate("personal_newAccount",$password));
             var_dump($rep);
         }
 
     }
-    private function GetRandStr($length)
+    public function GetRandStr($length)
     {
         //字符组合
         $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -27,5 +30,30 @@ class newAccount
             $randstr .= $str[$num];
         }
         return $randstr;
+    }
+    public  function post($url, $data = [], $header = [])
+    {
+        $ch = curl_init();
+        $data = json_encode($data);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', 'Content-Length:' . strlen($data)]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result,1);
+    }
+    public  function createDate($method, $params)
+    {
+        return [
+            "jsonrpc" => "2.0",
+            "method" => $method,
+            "params" => $params,
+            "id" => intval(time() . rand(1000, 9999))
+        ];
     }
 }
